610113e620c7684d000beef3f7beb9869c006806
Sóstenes Santana Santos <sostenes.dev@outlook.com.br>
2024-12-05 23:41:46 -0300

    Versão desktop finalizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+      fontFamily: {
+        'primary': ['Gothic', 'Helvetica', 'Arial', 'sans-serif'],
+        'heading': ['Boowie', 'Helvetica', 'Arial', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          400: '#A35A61',
+          500: '#A35A61',
+          600: '#A35A61',
+        },
+        secondary: {
+          400: '#3A5367',
+          500: '#3A5367',
+          600: '#3A5367',
+        },
+        base: {
+          background: '#d6cfd3',
+          surface: '#d6cfd3',
+          border: '#000',
+        }
+      },
+      screens: {
+        'mobile': '640px',
+        'tablet': '768px',
+        'laptop': '1024px',
+        'desktop': '1280px',
+        'wide': '1536px'
+      },
+    },
   },
   plugins: [],
 }
